4f5127f08ee6f22475ae381f122b14b2a4581b94
Gustavo Bezerra <gustavo.estudo017@gmail.com>
2025-08-11 10:03:57 -0300

    Delete Web developer - Caio directory

--- Web developer - Caio/aula-11/src/Pages/PageNotFound.jsx
+++
@@ -1,7 +0,0 @@
-function PageNotFound() {
-    return ( 
-        <h1>N encontrado</h1>
-    );
-}
-
-export default PageNotFound;
--- Web developer - Caio/aula-8/src/componentes/rodape/Rodape.jsx
+++
@@ -1,9 +0,0 @@
-function Rodape(){
-    return (
-        <footer>
-            <p>&copy; 2024 Meu Site</p>
-        </footer>
-    )
-}
-
-export default Rodape
